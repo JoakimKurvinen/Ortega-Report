4525caea41dc7bd6d77457fd590ce398189f5fd4
Leo Vaario <leovaa@metropolia.fi>
2017-12-08 12:20:43 +0200

    swarm rdy for use

--- BigManAsteroids_Full/Assets/Scripts/SwarmParent.cs
+++ BigManAsteroids_Full/Assets/Scripts/SwarmParent.cs
@@ -15,4 +15,34 @@ public class SwarmParent : MonoBehaviour {
 	void Update () {
 		
 	}
+    GameObject FindTarget(string targetTag)
+    {
+        GameObject[] potTargets;
+        List<float> potDistance = new List<float>();
+        int IndexMin = 0;
+
+        //PUTTING ALL POTENTIONAL TARGETS INTO LIST OF GAMEOBJECTS
+        potTargets = GameObject.FindGameObjectsWithTag(targetTag);
+
+        if (potTargets.Length == 0)
+        {
+            return null;
+        }
+
+        foreach (GameObject n in potTargets)//Checking for distance to each object
+        {
+            potDistance.Add((n.transform.position - transform.position).magnitude);
+        }
+
+        //FINDING SMALLEST DISTANCE VALUE
+
+        for (int m = 0; m < potTargets.Length; m++)
+        {
+            if (potDistance[m] < potDistance[IndexMin])    //compare Min with every value,
+            {
+                IndexMin = m;   //if smaller value is found, set Min to smaller value
+            }
+        }
+        return (potTargets[IndexMin]);
+    }
 }
