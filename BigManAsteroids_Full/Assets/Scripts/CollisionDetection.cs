7c9bb1ee027a0323cf8b59eac5807d51533961ef
Aleksi <aleksi.martikainen@metropolia.fi>
2017-12-13 10:59:58 +0200

    error

--- BigManAsteroids_Full/Assets/Scripts/CollisionDetection.cs
+++ BigManAsteroids_Full/Assets/Scripts/CollisionDetection.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionDetection : MonoBehaviour {
 
+    //private GameObject Enemy;
+   // private int RemainingEnemies = 11;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //Enemy = GameObject.Find("small_ships (1)(Clone)");
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        
-	}
+/*	void Update () {
+        if (RemainingEnemies == 0)
+        {
+            SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+        }
+    }
+
+    void NextLevel()
+    {
+        Debug.Log("Red destroyed"); */
+        /*if (Enemy == null)
+        {
+            SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+        }*/
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         /*if  (
@@ -22,7 +38,10 @@ public class CollisionDetection : MonoBehaviour {
         {
             Physics2D.IgnoreCollision(this, collision);
         }*/
-        
+       /* if (this.gameObject.name == ("bullet(Clone)") && collision.gameObject.name == ("small_ships (1)(Clone)"))
+        {
+            RemainingEnemies -= 1;
+         */
 
 
         if  (
@@ -30,11 +49,12 @@ public class CollisionDetection : MonoBehaviour {
 
             ||
             
-                (this.gameObject.tag == ("¨Red") && collision.gameObject.tag == ("Blue"))
+                (this.gameObject.tag == ("Red") && collision.gameObject.tag == ("Blue"))
             )
         {
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
+            NextLevel();
         }
     }
 }
